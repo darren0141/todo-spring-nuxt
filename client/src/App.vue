cddaae438af141805c7cc40b3b477376e19530c8
darren0141 <darrencowan95@gmail.com>
2022-05-28 12:45:35 +0100

    add App.vue html code

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Todos />
+      <footer class="info">
+        <p>Based on a project written by <a href="http://evanyou.me">Evan You</a></p>
+      <p>Original Vue TodoApp project is <a href="https://vuejs.org/v2/examples/todomvc.html">here</a></p>
+      <p>Modified for this tutorial by Andrew Hughes</p>
+    </footer>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import Todos from './components/Todos'
+  // app Vue instance
+  const app = {
+    name : 'app',
+    components : {
+      Todos
+    },
+    // app initial state
+    data : () => {
+      return {
+      }
+    }
   }
-}
+
+  export defauly app
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  [v-cloak] { display : none; }
+</script>
