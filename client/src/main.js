c467903f25d92d665c65b3c4882164b43d37dcbe
darren0141 <darrencowan95@gmail.com>
2022-05-28 12:36:44 +0100

    update main.js with logging options

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue'
+import App from './App'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false
+
+import VueLogger from 'vuejs-logger'
+
+const options = {
+    isEnabled : true,
+    logLevel : 'debug',
+    stringifyArguments : false,
+    showLogLevel : true,
+    showMethodName : false,
+    separator : '|',
+    showConsoleColors : true
+};
+
+Vue.use(VueLogger, options);
+
+/*eslint-disable no-new*/
+new Vue({
+    el : '#app',
+    template : '<App/>',
+    components : { App }
+});
